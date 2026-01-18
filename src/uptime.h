a59c2839dd288688fe6718abff7e73354014589e
bozont <884528-bozont91@users.noreply.gitlab.com>
2026-01-18 22:18:13 +0100

    Add license and function descriptions to the header file

--- src/uptime.h
+++ src/uptime.h
@@ -1,3 +1,31 @@
+/*
+MIT License
+
+bozontlabsUptime - Uptime tracking library for Arduino devices
+Copyright (c) 2026 Tamas Jozsi
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+This permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #pragma once
 
 #include <Arduino.h>
@@ -10,6 +38,7 @@ public:
     return instance;
   }
 
+  // Updates the uptime tracker and checks for millis() overflows
   void update() {
     static uint32_t millis_prev = 0u;
     // Check if millis has overflowed
@@ -19,26 +48,31 @@ public:
     millis_prev = millis();
   }
 
+  // Returns the days since last reset
   uint64_t get_uptime_days() {
     uint64_t uptime_days = ((uint64_t)millis() + (this->millis_overflows * UINT32_MAX)) / (uint64_t)86400000;
     return uptime_days;
   }
 
+  // Returns the hours since last reset
   uint64_t get_uptime_hours() {
     uint64_t uptime_hours = ((uint64_t)millis() + (this->millis_overflows * UINT32_MAX)) / (uint64_t)3600000;
     return uptime_hours;
   }
 
+  // Returns the minutes since last reset
   uint64_t get_uptime_minutes() {
     uint64_t uptime_minutes = ((uint64_t)millis() + (this->millis_overflows * UINT32_MAX)) / (uint64_t)60000;
     return uptime_minutes;
   }
 
+  // Returns the seconds since last reset
   uint64_t get_uptime_seconds() {
     uint64_t uptime_seconds = ((uint64_t)millis() + (this->millis_overflows * UINT32_MAX)) / (uint64_t)1000;
     return uptime_seconds;
   }
 
+  // Returns a human-readable uptime string like '5d 7h 24m 12s'
   String get_uptime_string() {
     uint64_t up_days = this->get_uptime_days();
     uint64_t up_hours = this->get_uptime_hours() % 24;
@@ -53,6 +87,7 @@ public:
     return up_str;
   }
 
+  // Returns the milliseconds since last reset
   uint64_t get_uptime_milliseconds() {
     return (uint64_t)millis() + (this->millis_overflows * UINT32_MAX);
   }
